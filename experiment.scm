fdbc479591175e2e900bee566d4037bed8aa600b
Alex <alexander.soto@gmail.com>
2019-06-23 11:12:03 -0400

    experiment

---
+++ experiment.scm
@@ -0,0 +1,28 @@
+(use-modules (gnu packages)
+             (guix profiles)
+             (ice-9 match)
+             (ice-9 pretty-print))
+
+
+;;; HELPER PROCEDURES
+;;; =================
+
+(define (profile-manifest->package-names)
+  "Return a list with the names of the packages installed in the user
+   profile (see %user-profile-directory in the (guix profiles) module)."
+  (map manifest-entry-name
+       (manifest-entries (profile-manifest %user-profile-directory))))
+
+
+;;; PACKAGES
+;;; ========
+
+(define packages
+  (append (profile-manifest->package-names)
+          (list "zathura"))
+
+
+;;; MANIFEST
+;;; ========
+
+(packages->manifest (map specification->package packages))
