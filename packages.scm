db871285ff256ca7bd86956dab0f94832635c316
Alex <alexander.soto@gmail.com>
2019-06-22 22:40:21 -0400

    updating config

--- packages.scm
+++ packages.scm
@@ -1,27 +1,3 @@
-;;; packages.scm --- Guix packages I use
-
-;; Copyright © 2015–2018 Alex Kost <alezost@gmail.com>
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;; Lists of packages that I install in my Guix profiles using
-;; "guix package --manifest" facility.
-
-;;; Code:
-
 (define-module (Public user-packages)
   #:use-module (Public utils)
   #:export (build-common-packages
