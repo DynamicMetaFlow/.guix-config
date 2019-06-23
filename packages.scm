6d9cc2c3beef59cd9462829142c61c29b06d87e7
Alex <alexander.soto@gmail.com>
2019-06-22 22:57:16 -0400

    tangle

--- packages.scm
+++ packages.scm
@@ -1,61 +1,12 @@
-(use-modules (guix packages))
+(use-modules (guix packages)
+             (guix git-download)
+             (gnu packages linux))
+
+(use-package-modules emacs)
+
+
 
 
 (packages->manifest
- (list alsa-utils
-       aseprite
-       emacs
-       emacs-better-defaults
-       emacs-elfeed
-       emacs-ido-ubiquitous
-       emacs-js2-mode
-       emacs-markdown-mode
-       emacs-projectile
-       emacs-rainbow-delimiters
-       emacs-smex
-       emacs-web-mode
-       emacs-yaml-mode
-       eog
-       evince
-       file-roller
-       font-adobe-source-han-sans
-       font-dejavu
-       font-inconsolata
-       geda-gaf
-       geiser-next
-       gimp
-       git
-       (list git "send-email")
-       git-manpages
-       gmtp
-       gnome-tweak-tool
-       gnupg
-       guile-2.0
-       icecat
-       inkscape
-       livestreamer
-       magit
-       milkytracker
-       mpd
-       mpdscribble
-       mpv
-       ncmpcpp
-       notmuch
-       obs
-       offlineimap
-       paredit
-       pavucontrol
-       pcb
-       pinentry
-       pwgen
-       screen
-       scrot
-       shepherd
-       skribilo
-       sshfs-fuse
-       strace
-       (list transmission "gui")
-       typo
-       wget
-       xmodmap
-       youtube-dl))
+ (list emacs
+       emacs-better-defaults))
